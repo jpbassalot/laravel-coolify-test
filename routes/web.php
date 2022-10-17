9e01ce8ff8ba1e1a1acac4d6e77671cd7ef1ca6a
Joseph C White <joseph.white@kccd.edu>
2022-10-17 14:41:01 -0700

    commit a change and see if app builds like it should

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HelloWorldController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Home page - HelloWorldController@index
+Route::get('/', [HelloWorldController::class, 'index']);
